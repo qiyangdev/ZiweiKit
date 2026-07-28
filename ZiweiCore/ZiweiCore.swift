eaf8e32d6352d2d4d9de6421d29f02c6f43e61d5
qiyang <wangqiyangx@gmail.com>
2026-07-28 16:46:19 +0800

    Initial Commit

---
+++ ZiweiCore/ZiweiCore.swift
@@ -0,0 +1,9 @@
+//
+//  ZiweiCore.swift
+//  ZiweiCore
+//
+//  Created by qiyang on 2026/7/28.
+//
+
+import Foundation
+
---
+++ ZiweiCoreTests/ZiweiCoreTests.swift
@@ -0,0 +1,19 @@
+//
+//  ZiweiCoreTests.swift
+//  ZiweiCoreTests
+//
+//  Created by qiyang on 2026/7/28.
+//
+
+import Testing
+@testable import ZiweiCore
+
+struct ZiweiCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
